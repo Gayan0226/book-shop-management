928ee2d1fca2f62fb26ec24f3c90208089ed22e9
gayan <gcdisanayaka1998@gmail.com>
2024-07-08 23:49:42 +0530

    set Properties

---
+++ src/main/java/com/bookshop/book_shop_management/BookShopManagementApplication.java
@@ -0,0 +1,13 @@
+package com.bookshop.book_shop_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShopManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookShopManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookshop/book_shop_management/BookShopManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookshop.book_shop_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShopManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
