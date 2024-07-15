bfeba70e3fc8226f8176830145b8f1873349b54b
gayan <gcdisanayaka1998@gmail.com>
2024-07-15 09:41:16 +0530

    entity finished

--- src/main/java/com/bookshop/book_shop_management/controller/AuthorController.java
+++ src/main/java/com/bookshop/book_shop_management/controller/AuthorController.java
@@ -1,11 +1,13 @@
 package com.bookshop.book_shop_management.controller;
 
+import com.bookshop.book_shop_management.entity.Author;
 import com.bookshop.book_shop_management.service.AuthorService;
+import com.bookshop.book_shop_management.util.StandardResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/author-controller")
@@ -14,6 +16,15 @@ public class AuthorController {
     @Autowired
     private AuthorService authorService;
     @PostMapping(path = {"/add-author"})
+    public ResponseEntity<StandardResponse> addAuthor(@RequestBody Author author) {
+        return new ResponseEntity<>(
+                new StandardResponse(
+                        200,
+                        "Add successfull",
+                        "1"
+                ), HttpStatus.CREATED
+        );
+    }
 
 
 
