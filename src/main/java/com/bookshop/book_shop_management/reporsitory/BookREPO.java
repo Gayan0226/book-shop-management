a01a23c102c62f64a0cb7238d7c938ef1e2a2ffe
DMGCD <gcdisanayaka1998@gmail.com>
2024-08-01 17:01:02 +0530

    add lagback file add the d:/springboot folder

--- src/main/java/com/bookshop/book_shop_management/reporsitory/BookREPO.java
+++ src/main/java/com/bookshop/book_shop_management/reporsitory/BookREPO.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-@EnableJpaRepositories
+//@EnableJpaRepositories
 @Transactional
 public interface BookREPO extends JpaRepository<Book, String> {
 
