43367f7a274e1957ddcac94a874a47def40efb08
gayan <gcdisanayaka1998@gmail.com>
2025-01-20 14:31:09 +0530

    remove basic Authentication  and Access it only Using Jwt Authentications  add successfully

--- src/main/java/com/bookshop/book_shop_management/config/SecurityConfig.java
+++ src/main/java/com/bookshop/book_shop_management/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -45,8 +44,6 @@ public class SecurityConfig {
                         .anyRequest()
                         .authenticated()
                 )
-//                .httpBasic(Customizer.withDefaults())
-
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(onceFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
